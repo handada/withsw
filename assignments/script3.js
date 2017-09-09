0d2794e6141093139763c1bbf7d4a113def8b63b
handada <31514625+handada@users.noreply.github.com>
2017-09-09 23:28:38 +0900

    9.9 11:28

--- assignments/script3.js
+++ assignments/script3.js
@@ -9,12 +9,4 @@ btn.addEventListener('click',function(){
 	var answer = document.getElementById('answer');
 	var result = add(v1,v2,v3);
 	answer.value = result;
-
-	if(answer.value <=7){
-		document.getElementById('picture').src="images/andae.jpg";
-	}else if(answer.value>7 && answer.value <=23){
-		document.getElementById('picture').src="images/taltal.jpg";
-	}else {
-		document.getElementById('picture').src="images/gae.jpg";
-	}
 });
