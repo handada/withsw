6ef0644d1033580f23f21ed6626d831e0f4efe58
handada <31514625+handada@users.noreply.github.com>
2017-09-13 15:24:06 +0900

    9.13

--- assignments/script3.js
+++ assignments/script3.js
@@ -1,12 +1,26 @@
+
 function add(a,b,c){
 	return a + b + c;
 }
-btn = document.getElementById('bnt');
-btn.addEventListener('click',function(){
+function total(){
 	var	v1 = Number(document.getElementById('first').value);
 	var	v2 = Number(document.getElementById('second').value);
 	var	v3 = Number(document.getElementById('third').value);
-	var answer = document.getElementById('answer');
-	var result = add(v1,v2,v3);
-	answer.value = result;
-});
+	var result = document.getElementById('result');
+	var p = document.querySelector('p');
+	if (isNaN(v1+v2+v3)){
+			p.style.fontColor="red";
+		}else{
+			var sum = add(v1,v2,v3);
+			result.value = sum;
+		}
+};
+var btn = document.getElementById('bnt');
+btn.addEventListener('click', total);
+
+(function(){
+	var aler = document.getElementsByTagName("input");
+	aler[0].addEventListener('change', total);
+	aler[1].addEventListener('change', total);
+	aler[2].addEventListener('change', total);
+})();
