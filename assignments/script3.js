96641001d55c11cbb429b5dd4fcc54a990316f68
handada <31514625+handada@users.noreply.github.com>
2017-09-13 20:50:56 +0900

    9.13 after

--- assignments/script3.js
+++ assignments/script3.js
@@ -1,26 +1,21 @@
-
+//총합계를 구하는 함수 add 생성
 function add(a,b,c){
 	return a + b + c;
 }
-function total(){
-	var	v1 = Number(document.getElementById('first').value);
-	var	v2 = Number(document.getElementById('second').value);
-	var	v3 = Number(document.getElementById('third').value);
-	var result = document.getElementById('result');
-	var p = document.querySelector('p');
-	if (isNaN(v1+v2+v3)){
-			p.style.fontColor="red";
-		}else{
-			var sum = add(v1,v2,v3);
-			result.value = sum;
-		}
+function check(){
+var	v1 = Number(document.getElementById('first').value);
+var	v2 = Number(document.getElementById('second').value);
+var	v3 = Number(document.getElementById('third').value);
+//경고 요소 값 담기
+var p = document.getElementById('ready');
+var result = document.getElementById('result');
+
+result.value = add(v1,v2,v3);
 };
-var btn = document.getElementById('bnt');
-btn.addEventListener('click', total);
 
 (function(){
-	var aler = document.getElementsByTagName("input");
-	aler[0].addEventListener('change', total);
-	aler[1].addEventListener('change', total);
-	aler[2].addEventListener('change', total);
+	var inputTag = document.getElementsByTagName("input");
+	inputTag[0].addEventListener('change', check);
+	inputTag[1].addEventListener('change', check);
+	inputTag[2].addEventListener('change', check);
 })();
