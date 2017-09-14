356b2b696f1efac04052eb6ddf391d612f236ce8
handada <31514625+handada@users.noreply.github.com>
2017-09-14 14:55:24 +0900

    9.14 r과제2 1차 완료
    숫자 외 입력시 경고글자 보이고 클릭시 총합 구해짐.

--- assignments/script3.js
+++ assignments/script3.js
@@ -1,18 +1,24 @@
-//총합계를 구하는 함수 add 생성
 function add(a,b,c){
 	return a + b + c;
 }
+var pTag = document.getElementById('ready');
+		pTag.style.color = "black";
 function check(){
-var	v1 = Number(document.getElementById('first').value);
-var	v2 = Number(document.getElementById('second').value);
-var	v3 = Number(document.getElementById('third').value);
-//경고 요소 값 담기
-var p = document.getElementById('ready');
-var result = document.getElementById('result');
+	var	v1 = Number(document.getElementById('first').value);
+	var	v2 = Number(document.getElementById('second').value);
+	var	v3 = Number(document.getElementById('third').value);
+	var result = document.getElementById('result');
 
-result.value = add(v1,v2,v3);
-};
-
+		if(isNaN(v1+v2+v3)){
+			pTag.style.color = "red";
+			pTag.innerText = "숫자만 입려해라 제발"
+			pTag.className = "redBorder";
+		}else{
+			btn = document.getElementById('bnt');
+			btn.addEventListener('click',function(){
+			 result.value=add(v1,v2,v3);
+		 	});
+				}};
 (function(){
 	var inputTag = document.getElementsByTagName("input");
 	inputTag[0].addEventListener('change', check);
